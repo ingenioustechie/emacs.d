689795c7c1aa2d468c1b72a95e39b763f604cf0d
IngeniousTechie <mukesh@ingenioustechie.com>
2015-01-07 14:38:45 +0530

    Chnaged in mode line

--- init.el
+++ init.el
@@ -79,9 +79,9 @@
 (require 'init-paredit)
 (require 'init-lisp)
 (require 'init-slime)
-(require 'init-clojure)
-(when (>= emacs-major-version 24)
-  (require 'init-clojure-cider))
+;;(require 'init-clojure)
+;;(when (>= emacs-major-version 24)
+;;  (require 'init-clojure-cider))
 (require 'init-common-lisp)
 
 (when *spell-check-support-enabled*
