73ddadd53efcfd755a1d7a1b5f6bcb6dad197890
IngeniousTechie <mukesh@ingenioustechie.com>
2015-02-11 16:21:17 +0530

    Changed javascript indent to 4

--- custom.el
+++ custom.el
@@ -8,9 +8,9 @@
 
 ;; Standard Jedi.el setting
 (require 'epc)
+;(setq jedi:setup-keys t)
 (setq jedi:complete-on-dot t)
-(setq jedi:setup-keys t)
-(setq jedi:server-command '("python" "~/.emacs.d/elpa/jedi-20150109.2230/jediepcserver.py"))
+(setq jedi:server-command '("python" "/home/mak/.emacs.d/elpa/jedi-20150109.2230/jediepcserver.py"))
 (setq jedi:tooltip-method '(pos-tip))
 (autoload 'jedi:setup "jedi" nil t)
 (add-hook 'python-mode-hook 'jedi:setup)
@@ -20,3 +20,4 @@
 ;;     M-x package-install RET jedi RET
 ;;     M-x jedi:install-server RET
 ;; Then open Python file.
+
